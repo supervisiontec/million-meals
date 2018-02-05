dfeff3164960150364638a29f173abdaa9a4ee09
Thilina Kalum <kalum323@gmail.com>
2018-02-05 12:55:24 +0530

    popup main
    popup main

--- MillionMeals/src/site/app/main/main.js
+++ MillionMeals/src/site/app/main/main.js
@@ -1,12 +1,26 @@
 (function () {
     //controller
     angular.module("appModule")
-            .controller("mainController", function ($scope) {
+            .controller("mainController", function ($scope,optionPane,$uibModal) {
                 //data models 
                 $scope.model = {};
 
                 //ui models
                 $scope.ui = {};
+
+                $scope.ui.clickMe = function () {
+                                   console.log('click Me');        
+                            $uibModal.open({
+                                animation: true,
+                                ariaLabelledBy: 'modal-title',
+                                ariaDescribedBy: 'modal-body',
+                                templateUrl: 'item_selection_popup.html',
+                                scope: $scope,
+                                size: 'md'
+                            });
+                        
+                    
+                };
 
 
                 $scope.ui.init = function () {
